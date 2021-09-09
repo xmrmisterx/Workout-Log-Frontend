ac343cbb009d997c2da871fa2b62511c43bc9802
Bobby Nguyen <nguyenbo@flip1.engr.oregonstate.edu>
2021-09-08 21:19:40 -0700

    moved notes from server.js to Notes and removed some commented out notees

--- server.js
+++ server.js
@@ -29,15 +29,15 @@ app.use(function(req,res){
     res.render('404');
   });
   
-  app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-  });
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
 
-  // set listen port
-  
-  app.listen(app.get('port'), function(){
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-  });
+// set listen port
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
 
